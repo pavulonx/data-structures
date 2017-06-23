532a26b8f8854e9c329007416bfa5a3f877727df
Jakub Rozenbajger <thequetran@gmail.com>
2017-06-23 19:45:48 +0200

    initial commit

--- src/pl/rozen/Main.java
+++ src/pl/rozen/Main.java
@@ -7,49 +7,51 @@ import pl.rozen.tree.BSTree;
 public class Main {
 
     public static void main(String[] args) {
-//        BSTree<Integer, String> bst = new BSTree<>();
-//        bst.insert(6, "AAA");
-//        bst.insert(2, "BBB");
-//        bst.insert(3, "CCC");
-//        bst.insert(1, "DDD");
-//        Integer data = 8;
-//        bst.insert(data, "EEE");
-//        bst.insert(7, "FFF");
-//        bst.insert(9, "FFF");
-//        bst.insert(6, "XXX");
-//        bst.inOrderTraversal();
-//        System.out.println();
-//        System.out.println(bst);
-//        bst.delete(6);
-//        System.out.println(bst);
-//
-//        System.out.println(bst.get(data));
-//        System.out.println(bst.get(12));
-//        System.out.println(bst.get(null));
-//        bst.clear();
-//        bst.inOrderTraversal();
+
+        //// Showcase /////
+        BSTree<Integer, String> bst = new BSTree<>();
+        bst.insert(6, "AAA");
+        bst.insert(2, "BBB");
+        bst.insert(3, "CCC");
+        bst.insert(1, "DDD");
+        Integer data = 8;
+        bst.insert(data, "EEE");
+        bst.insert(7, "FFF");
+        bst.insert(9, "FFF");
+        bst.insert(6, "XXX");
+        bst.inOrderTraversal();
+        System.out.println();
+        System.out.println(bst);
+        bst.delete(6);
+        System.out.println(bst);
+
+        System.out.println(bst.get(data));
+        System.out.println(bst.get(12));
+        System.out.println(bst.get(null));
+        bst.clear();
+        bst.inOrderTraversal();
 
 
-//        HashMap<Integer, String > hm = new HashMap<>();
-//        hm.put(1,"1");
-//        hm.put(2,"2");
-//        hm.put(3,"3");
-//        hm.put(4,"4");
-//        hm.put(5,"5");
-//        hm.put(6,"6");
-//        System.out.println(hm.get(2));
-//        hm.remove(6);
-//        System.out.println(hm.get(2));
-//        hm.remove(2);
-//        hm.remove(1);
-//        System.out.println(hm.get(2));
-//        for (int i = 0; i < 20; i++) {
-//            hm.put(i,Integer.toString(i));
-//        }
-//        hm.clear();
-//        for (int i = 0; i < 20; i++) {
-//            hm.put(i,Integer.toString(i));
-//        }
+        HashMap<Integer, String > hm = new HashMap<>();
+        hm.put(1,"1");
+        hm.put(2,"2");
+        hm.put(3,"3");
+        hm.put(4,"4");
+        hm.put(5,"5");
+        hm.put(6,"6");
+        System.out.println(hm.get(2));
+        hm.remove(6);
+        System.out.println(hm.get(2));
+        hm.remove(2);
+        hm.remove(1);
+        System.out.println(hm.get(2));
+        for (int i = 0; i < 20; i++) {
+            hm.put(i,Integer.toString(i));
+        }
+        hm.clear();
+        for (int i = 0; i < 20; i++) {
+            hm.put(i,Integer.toString(i));
+        }
 
 
         LinkedList<Integer> list = new LinkedList<>();
@@ -65,6 +67,6 @@ public class Main {
         list.get(3);
         list.get(-1);
         list.get(1020);
-        
+
     }
 }
